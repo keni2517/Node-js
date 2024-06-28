e5506d7f539fab93e6bbed2a161bfb8c13367e71
keni <kenikakadiya@gmail.com>
2024-06-28 08:14:22 +0530

    28/6/24

---
+++ MYSQL/Lectures/lecture1.sql
@@ -0,0 +1,40 @@
+=>sql enter
+---\sql
+
+=>connect mysql :-
+---\connect root@localhost:3306   
+---password:-Admin@123
+
+=>show all databases command :-
+---show databases;
+
+=>create database command :- 
+---create database (databasename);
+--- Ex:- create database student;
+
+=>use perticular database command :- 
+---use (databasename);
+--- Ex:-use student;
+
+=>database delete :- 
+---drop database databasename;
+--- Ex:-drop database student;
+
+=>create table :-
+---create table tablename(
+--- filedname filedtype
+---);
+
+--- Ex:-create table user(
+--- firstname varchar(30)
+--- lastname varchar(30)
+--- age int
+---);
+
+=>show perticular database table :- 
+---show tables;
+
+=>describe or show perticular table(row and column)
+---desc tablename;
+---describe tablename;
+--- Ex:-describe user;
